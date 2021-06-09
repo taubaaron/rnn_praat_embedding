ba4443c48426b3b30492050c5e0115cffef31a94
Aaron taub <ataub@apple.com>
2021-06-09 12:29:01 +0300

    first commit

---
+++ praat_lpc_functions.py
@@ -0,0 +1,162 @@
+import parselmouth
+import scipy.signal
+from IPython.display import Audio
+from parselmouth.praat import call
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_sound(sound):
+    plt.figure()
+    plt.plot(sound.xs(), sound.values.T)
+    plt.xlim([sound.xmin, sound.xmax])
+    plt.xlabel("Time [sec]")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+def draw_spectrogram(spectrogram, dynamic_range=70):
+    X, Y = spectrogram.x_grid(), spectrogram.y_grid()
+    sg_db = 10 * np.log10(spectrogram.values)
+    # plt.pcolormesh(X, Y, sg_db, vmin=sg_db.max() - dynamic_range, cmap='afmhot')
+    plt.pcolormesh(X, Y, sg_db, vmin=sg_db.max() - dynamic_range)
+    plt.ylim([spectrogram.ymin, spectrogram.ymax])
+    plt.xlabel("time [s]")
+    plt.ylabel("frequency [Hz]")
+
+def draw_intensity(intensity):
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=3, color='w')
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=1)
+    plt.grid(False)
+    plt.ylim(0)
+    plt.ylabel("intensity [dB]")
+
+def draw_pitch(pitch):
+    # Extract selected pitch contour, and
+    # replace unvoiced samples by NaN to not plot
+    pitch_values = pitch.selected_array['frequency']
+    pitch_values[pitch_values == 0] = np.nan
+    plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='w')
+    plt.plot(pitch.xs(), pitch_values, 'o', markersize=2)
+    plt.grid(False)
+    plt.ylim(0, pitch.ceiling)
+    plt.ylabel("fundamental frequency [Hz]")
+
+
+def pitch_manipulation(sound):
+    manipulation = call(sound, "To Manipulation", 0.01, 75, 600)
+    pitch_tier = call(manipulation, "Extract pitch tier")
+
+    call(pitch_tier, "Multiply frequencies", sound.xmin, sound.xmax, 2)
+    call([pitch_tier, manipulation], "Replace pitch tier")
+    sound_octave_up = call(manipulation, "Get resynthesis (overlap-add)")
+    Audio(data=sound_octave_up.values, rate=sound_octave_up.sampling_frequency)
+    sound_octave_up.save("whitney houston.wav", "WAV")
+    Audio(filename="whitney houston.wav")
+
+
+def lpc_extract(sound):
+    lpc = parselmouth.praat.call(sound, "To LPC (burg)", 64, 0.025, 0.005, 50.0)
+    lpc_matrix = parselmouth.praat.call(lpc, "Down to Matrix (lpc)")
+    print(lpc_matrix.values)
+
+
+def mixing_voices(sound_a, sound_b):
+    """
+    !!Verify Capitalized Letters!!
+
+    Whitney:                               Analyse periodicity -> to_pitch
+    whitney.to_pitch:                      sound -> to_sound_pulses
+    to_sound_pulses:                       lpf (?)
+    Aaron:                                 Analyse Spectrum -> to_lpc(burg), 64 co, 0.025 w, ts 0.05, pre-emph 50
+    Aaron_lpc + to_sound_pulses_lpf:       filter
+    result:                                modify -> normalize/scale_peak
+    """
+    sound_a_pitch = sound_a.to_pitch()
+    sound_a_pitch_pulse = sound_a_pitch.to_sound_pulses()
+    sound_a_pitch_pulse.save("sound_a_pitch_pulse.wav", "WAV")
+    Audio(filename="sound_a_pitch_pulse.wav")
+
+    """   # LPF
+    order = 5
+    sampling_freq = 30
+    cutoff_freq = 2
+
+    normalized_cutoff_freq = 2 * cutoff_freq / sampling_freq
+    numerator_coeffs, denominator_coeffs = scipy.signal.butter(order, normalized_cutoff_freq)
+    sound_a_pitch_pulse_lpf = scipy.signal.lfilter(numerator_coeffs, denominator_coeffs, sound_a_pitch_pulse)
+    sound_a_pitch_pulse_lpf.save("sound_a_pitch_pulse_lpf.wav", "WAV")
+    Audio(filename="sound_a_pitch_pulse_lpf.wav")"""
+
+    sound_b_lpc = parselmouth.praat.call(sound_b, "To LPC (burg)", 64, 0.025, 0.005, 50.0)
+    sound_b_lpc_matrix = parselmouth.praat.call(sound_b_lpc, "Down to Matrix (lpc)")
+
+
+
+    result = parselmouth.praat.call([sound_a_pitch_pulse, sound_b_lpc], "Filter...", False)
+
+    # result_scaled = parselmouth.praat.call(result, "Scale peak...", 0.99)
+    result_scaled = parselmouth.praat.call(result, "Scale peak...", 0.99)
+
+    result_scaled.save("result_scaled.wav", "WAV")
+    Audio(filename="result_scaled.wav")
+
+    # print(sound_b_lpc_matrix.values)
+
+
+
+
+if __name__ == '__main__':
+    # Load Songs
+    sound = parselmouth.Sound("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/whitney houston.wav")
+
+    # Prepare plotting
+    sns.set()  # default style for graphs
+    plt.rcParams['figure.dpi'] = 100  # show large images
+
+    # plot_sound(sound)  # plotting the sound
+    #
+    # # Extract a part of the song
+    # sound_part = sound.extract_part(from_time=0.9, preserve_times=True)
+    # plot_sound(sound_part)
+
+    # intensity = sound.to_intensity()
+    # spectrogram = sound.to_spectrogram()
+    # plt.figure()
+    # draw_spectrogram(spectrogram)
+    # plt.twinx()
+    # draw_intensity(intensity)
+    # plt.xlim([sound.xmin, sound.xmax])
+    # plt.show()
+    #
+    # pitch = sound.to_pitch()
+    # # If desired, pre-emphasize the sound fragment before calculating the spectrogram
+    # pre_emphasized_snd = sound.copy()
+    # pre_emphasized_snd.pre_emphasize()
+    # spectrogram = pre_emphasized_snd.to_spectrogram(window_length=0.03, maximum_frequency = 8000)
+    #
+    # plt.figure()
+    # draw_spectrogram(spectrogram)
+    # plt.twinx()
+    # draw_pitch(pitch)
+    # plt.xlim([sound.xmin, sound.xmax])
+    # plt.show()
+
+
+    # my functions
+
+    # pitch_manipulation(sound)
+    # Audio(data=sound.values, rate=sound.sampling_frequency)
+
+    # LPC Extraction
+    # lpc_extract(sound)
+
+    sound_a = parselmouth.Sound("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/whitney houston.wav")
+    sound_b = parselmouth.Sound("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/ExampleWithLPC/Audacity/fixed - Aaron talking whitney 2.wav")
+
+    # mixing_voices(sound_a, sound_b)
+
+
+
+
+
---
+++ voice_embeddings.py
@@ -0,0 +1,153 @@
+from resemblyzer import VoiceEncoder, preprocess_wav
+from pathlib import Path
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from scipy.stats import mode
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+import os
+import umap.umap_ as umap
+from itertools import groupby
+from tqdm import tqdm
+
+
+def to_embedding(path):
+    fpath = path
+    wav = preprocess_wav(fpath)
+
+    encoder = VoiceEncoder()
+    embed = encoder.embed_utterance(wav)
+    np.set_printoptions(precision=3, suppress=True)
+    return embed
+
+
+def to_clustering(digits):
+    # Project the data: this step will take several seconds
+    tsne = TSNE(n_components=2, init='random', random_state=0)
+    digits_proj = tsne.fit_transform(digits[0])  # 1797 x 64 == num_examples x example_dim ours: examples x 256 => examples x 2
+    # Compute the clusters
+    kmeans = KMeans(n_clusters=10, random_state=0)
+    clusters = kmeans.fit_predict(digits_proj)
+
+    # Permute the labels
+    # labels = np.zeros_like(clusters)
+    # for i in range(10):
+    #     mask = (clusters == i)
+    #     labels[mask] = mode(digits[1])[0]
+
+    plt.scatter(digits_proj[:, 0], digits_proj[:, 1], s=50, cmap='viridis')
+
+
+def web_clustering(emb_mat):
+    # Project the data: this step will take several seconds
+    tsne = TSNE(n_components=2, init='random', random_state=0)
+    digits_proj = tsne.fit_transform(emb_mat)  # examples x dim => examples x 2
+
+    # Compute the clusters
+    kmeans = KMeans(n_clusters=10, random_state=0)
+    clusters = kmeans.fit_predict(digits_proj)
+
+    """    # Permute the labels
+    labels = np.zeros_like(clusters)
+    for i in range(10):
+        mask = (clusters == i)
+        labels[mask] = mode(target[mask])[0]
+    # Compute the accuracy
+    accuracy_score(target, clusters)
+    """
+    # plot
+    plt.scatter(digits_proj[:, 0], digits_proj[:, 1], c=clusters, s=50, cmap="tab10")
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+    plt.colorbar()
+    plt.show()
+
+
+def plot_projections(embeds, speakers, ax=None, markers=None, legend=True, title="", **kwargs):
+    if ax is None:
+        _, ax = plt.subplots(figsize=(6, 6))
+
+    # Compute the 2D projections. You could also project to another number of dimensions (e.g.
+    # for a 3D plot) or use a different different dimensionality reduction like PCA or TSNE.
+    reducer = umap.UMAP(**kwargs)
+    projs = reducer.fit_transform(embeds)
+
+    # Draw the projections
+    speakers = np.array(speakers)
+
+    colors_dict = {
+    "f1": [0/255, 0/255, 0/255],
+    "f2": [255/255, 0/255, 0/255],
+    "f3": [0/255, 255/255, 0/255],
+    "f4": [0/255, 0/255, 255/255],
+    "f5": [255/255, 255/255, 0/255],
+    "m1": [255/255, 0/255, 255/255],
+    "m2": [0/255, 255/255, 255/255],
+    "m3": [128/255, 0/255, 0/255],
+    "m4": [0/255, 128/255, 0/255],
+    "m5": [0/255, 128/255, 128/255],
+    }
+
+    for i, speaker in enumerate(np.unique(speakers)):
+        speaker_projs = projs[speakers == speaker]
+        marker = "o" if markers is None else markers[i]
+        label = speaker if legend else None
+        ax.scatter(*speaker_projs.T, c=[colors_dict[speaker]], marker=marker, label=label)
+
+    if legend:
+        ax.legend(title="Speakers", ncol=2)
+    ax.set_title(title)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_aspect("equal")
+
+    return projs
+
+
+def resemblyzer_clustering(wavs, speakers):
+    plot_projections(wavs, speakers, title="Embedding projections")
+    plt.show()
+
+
+if __name__ == "__main__":
+    embedding_vectors = []
+    speakers = []
+
+    directory = r'/Users/aarontaub/Desktop/specific_nhss'
+    for filename in os.listdir(directory):
+        if filename.endswith(".wav"):
+            embed = to_embedding(Path(f"{directory}/{filename}"))
+            embedding_vectors.append(embed)
+            speakers.append(filename[0:2])
+        else:
+            continue
+    print(embedding_vectors)
+    embedding_matrix = np.array(embedding_vectors)
+
+    # original code
+    # digits = load_digits()  # examples X dim
+    # web_clustering(digits.data)
+    # web_clustering(embedding_matrix)
+
+
+    resemblyzer_clustering(embedding_matrix, speakers)
+
+
+
+
+
+
+    # # Amit
+    # embed1 = to_embedding(Path("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/whitney houston.wav"))
+    # embed2 = to_embedding(Path("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/whitney houston.wav"))
+    # embed3 = to_embedding(Path("/Users/aarontaub/Google Drive/AaronAndAmitBIU/FinalProject/Praat/whitney houston.wav"))
+    #
+    # data = np.vstack( (embed1 , embed2) )
+    # data = np.vstack( (data , embed3) )
+    #
+    # target = np.array([1,1,1])
+    #
+    # web_clustering(data,target)
+
